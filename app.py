5617483efe4e40c001e6942b565b8a8f2498f0ad
alexis.moulin <26531613+alexismoulin@users.noreply.github.com>
2025-07-13 16:55:24 +0200

    routes simplified

--- app.py
+++ app.py
@@ -2,23 +2,35 @@ from flask import Flask, jsonify, request, Response, make_response, stream_with_
 from flask_cors import CORS
 import logging
 import queue
-from core.utils import sanitize_name, get_nifti_dimensions, list_folder_subfolders, get_folder_names
+from core.utils import sanitize_name, list_folder_subfolders
 from core.processing import STEP_COMPLETION_QUEUE, BASE_DATA_PATH, processing_event, run_processing, read_json_file
+from typing import Tuple
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 logger = logging.getLogger(__name__)
+
+
+def serve_json(patient: str, study: str, filename: str, err_msg: str) -> Tuple[Response, int]:
+    path = BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / "JSON" / filename
+    data = read_json_file(path)
+    if data:
+        return jsonify(data), 200
+    return jsonify({"error": err_msg}), 404
+
 
 @app.get("/")
 def home() -> str:
     """Basic home endpoint."""
     return "Home"
 
+
 @app.get("/stream")
 def stream() -> Response:
     """
     Stream processing step completions to the frontend in real time using Server-Sent Events (SSE).
     """
+
     @stream_with_context
     def event_stream():
         while True:
@@ -35,6 +47,7 @@ def stream() -> Response:
 
     headers = {"Cache-Control": "no-cache"}
     return Response(event_stream(), headers=headers, mimetype="text/event-stream")
+
 
 @app.post("/run_script")
 def run_script() -> Response:
@@ -61,6 +74,7 @@ def run_script() -> Response:
     response.status_code = 202
     return response
 
+
 @app.get("/studies")
 def studies() -> Response:
     couples = list_folder_subfolders(directory_path=BASE_DATA_PATH)
@@ -75,50 +89,26 @@ def studies() -> Response:
         response.status_code = 404
         return response
 
+
 @app.get("/cortical/<patient>/<study>")
-def cortical(patient: str, study: str) -> Response:
-    json_path = BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / "JSON" / "cortical.json"
-    cortical_json = read_json_file(json_path)
-    if cortical_json:
-        response = make_response(jsonify(cortical_json))
-        response.status_code = 200
-        return response
-    else:
-        response = make_response(jsonify({"error": "No Data"}))
-        response.status_code = 404
-        return response
+def cortical(patient: str, study: str) -> Tuple[Response, int]:
+    return serve_json(patient, study, filename="cortical.json", err_msg="No cortical data")
+
 
 @app.get("/subcortical/<patient>/<study>")
-def subcortical(patient: str, study: str) -> Response:
-    json_path = BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / "JSON" / "subcortical.json"
-    subcortical_json = read_json_file(json_path)
-    if subcortical_json:
-        return jsonify(subcortical_json)
-    else:
-        response = make_response(jsonify({"error": "No Data"}))
-        response.status_code = 404
-        return response
+def subcortical(patient: str, study: str) -> Tuple[Response, int]:
+    return serve_json(patient, study, filename="subcortical.json", err_msg="No subcortical data")
+
 
 @app.get("/general/<patient>/<study>")
-def general(patient: str, study: str) -> Response:
-    json_path = BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / "JSON" / "general.json"
-    general_json = read_json_file(json_path)
-    if general_json:
-        return jsonify(general_json)
-    else:
-        response = make_response(jsonify({"error": "No Data"}))
-        response.status_code = 404
-        return response
+def general(patient: str, study: str) -> Tuple[Response, int]:
+    return serve_json(patient, study, filename="general.json", err_msg="No general data")
+
 
 @app.get("/series/<patient>/<study>")
-def get_series(patient: str, study: str) -> Response:
-    dicoms = BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / "DICOM"
-    series_list = get_folder_names(dicoms)
-    series_dict = {
-        s: get_nifti_dimensions(BASE_DATA_PATH / sanitize_name(patient) / sanitize_name(study) / f"NIFTI/{s}.nii.gz")
-        for s in series_list
-    }
-    return jsonify(series_dict)
+def get_nifti_dim(patient: str, study: str) -> Tuple[Response, int]:
+    return serve_json(patient, study, filename="niftiDimensions.json", err_msg="No nifti dimension data")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
